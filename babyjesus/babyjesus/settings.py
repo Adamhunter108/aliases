4d9070d3291963cb30b6c1f2745dcd841df7f81d
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-05-18 15:04:58 -0400

    adding heroku deploy code

--- babyjesus/babyjesus/settings.py
+++ babyjesus/babyjesus/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for babyjesus project.
 
@@ -118,6 +120,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 LOGIN_REDIRECT_URL ='/'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
