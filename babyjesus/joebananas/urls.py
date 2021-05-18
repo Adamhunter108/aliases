38076b61bcc6ea228d4f01557b55bcb00ebf3d5d
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2021-05-18 10:25:07 -0400

    cleaned up code and added redirect to add name

--- babyjesus/joebananas/urls.py
+++ babyjesus/joebananas/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     # path('add/', views.add_name.as_view(), name='add'),  # trying class based views
-    path('add/', views.add_name, name='add_name'),
+    path('add/', views.get_add_name, name='get_add_name'),
+    path('add/post_add', views.post_add_name, name='post_add_name'),
     path("login/", views.login_view, name="login"),
     path("logout/", views.log_out, name="logout"),
     path("signup/", views.SignUp.as_view(), name="signup"),
--- babyjesus/joebananas/views.py
+++ babyjesus/joebananas/views.py
@@ -10,6 +10,9 @@ from django.contrib.auth.decorators import login_required
 import random
 from django.http import HttpResponse, HttpResponseNotFound, Http404,  HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib import messages
+
 
 def home(request):
 	all_names = Names.objects.all()
@@ -22,20 +25,15 @@ def home(request):
 	
 
 @login_required
-def add_name(request):
-	
-	model = Names
+def get_add_name(request):
 
 	form = AddNameForm(request.POST or None)
-
-	if form.is_valid():
-		form.save()
 
 	context = {
 		'form': form
 	}
 
-	print(request.POST)
+	# print(request.POST)
 	# checking the submission in the console
 
 	# return render(request, "joebananas/add_name.html", {'form': form})
@@ -43,6 +41,26 @@ def add_name(request):
 
 	return render(request, "joebananas/add_name.html", context)
 	
+
+@login_required
+def post_add_name(request):
+	
+	model = Names
+
+	form = AddNameForm(request.POST or None)
+
+	if form.is_valid():
+		form.save()
+		messages.success(request, 'Boom, added new alias successfully!')
+
+	context = {
+		'form': form
+	}
+
+	print(request.POST)
+
+	return redirect("get_add_name")
+
 
 def login_view(request):
 	username = request.POST["username"]
@@ -52,9 +70,9 @@ def login_view(request):
 	
 	if user is not None:
 		login(request, user)
-		render("joebananas/home.html")
+		render("home")
 	else:
-		render(request, "joebananas/login.html", context)
+		render(request, "login", context)
 
 
 def log_out(request):
